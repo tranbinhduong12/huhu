5c69c0c513636ec4537ea9a076ce679d266b7ef3
dwg1207 <tranduong12072001@gmail.com>
2023-04-14 00:12:48 +0700

    fig bux sign up

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -13,4 +13,25 @@ class RegisterController extends Controller
         // Trả về view cho trang chủ
         return view('register');
     }
+
+        /**
+     * Handle an authentication attempt.
+     */
+    public function authenticate(Request $request): RedirectResponse
+    {
+        $credentials = $request->validate([
+            'email' => ['required', 'email'],
+            'password' => ['required'],
+        ]);
+
+        if (Auth::attempt($credentials)) {
+            $request->session()->regenerate();
+
+            return redirect()->intended('dashboard');
+        }
+
+        return back()->withErrors([
+            'email' => 'The provided credentials do not match our records.',
+        ])->onlyInput('email');
+    }
 }
